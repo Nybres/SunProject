cd68b353ce7c5fd04578e046255964fbb19aa094
Pawel <ricowy96@gmail.com>
2022-06-05 12:28:21 +0200

    add header and features section

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,10 @@
+const nav = document.querySelector(".navbar");
 const navigation = document.querySelector(".navbar__container");
 const burgerBtn = document.querySelector(".burger");
 const burgerBars = document.querySelector(".burger__box-bar");
 
 const handleNav = params => {
+	nav.classList.toggle("navbar--active");
 	navigation.classList.toggle("navbar__container--active");
 	burgerBars.classList.toggle("burger__box-bar--active");
 };
